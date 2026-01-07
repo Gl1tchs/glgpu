28be436d57c815c9013d0ef6a667b8af357ef390
Gl1tchs <berkeumtbiricik@gmail.com>
2026-01-07 16:13:42 +0300

    add: script for versioning workflow.

---
+++ misc/bump_version.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Usage: ./bump_version.sh 1.2.3
+NEW_VERSION=$1
+
+if [ -z "$NEW_VERSION" ]; then
+    echo "Usage: ./bump_version.sh <major.minor.patch>"
+    exit 1
+fi
+
+# Update CMakeLists.txt
+# This searches for the project() line and replaces the version number
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS version of sed
+    sed -i '' "s/VERSION [0-9.]*/VERSION $NEW_VERSION/" CMakeLists.txt
+else
+    # Linux version of sed
+    sed -i "s/VERSION [0-9.]*/VERSION $NEW_VERSION/" CMakeLists.txt
+fi
+
+echo "Updated CMakeLists.txt to version $NEW_VERSION"
+
+# Git Workflow
+git add CMakeLists.txt
+git commit -m "chore: bump version to $NEW_VERSION"
+git tag -a "v$NEW_VERSION" -m "Version $NEW_VERSION"
+
+echo "Created git commit and tag v$NEW_VERSION"
+
+# Push to Remote
+read -p "Push to origin? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git push origin main --tags
+fi
+
+echo "Done!"
