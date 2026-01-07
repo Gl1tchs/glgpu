ca064f38df1d16b779f12e4b3530a55027f2690a
Gl1tchs <berkeumtbiricik@gmail.com>
2026-01-07 16:59:44 +0300

    chores: embedd version into code and log on device creation.

--- src/device.cpp
+++ src/device.cpp
@@ -1,12 +1,14 @@
 #include "glgpu/device.h"
 
+#include "glgpu/version.h"
 #include "platform/vulkan/vk_device.h"
 
 namespace gl {
 
 Res<std::shared_ptr<Device>> Device::create(const DeviceCreateInfo& info) {
-	std::shared_ptr<Device> backend = std::make_shared<VulkanDevice>();
+	GL_LOG_INFO("[GLGPU] Version {}", GLGPU_VERSION_STR);
 
+	std::shared_ptr<Device> backend = std::make_shared<VulkanDevice>();
 	if (auto r = backend->init(info); !r) {
 		return make_err<std::shared_ptr<Device>>(r.error());
 	}
