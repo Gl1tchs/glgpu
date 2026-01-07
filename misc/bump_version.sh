d3f1427e87e3d21dad6beb58271620c16b469f09
Gl1tchs <berkeumtbiricik@gmail.com>
2026-01-07 17:27:37 +0300

    chore: bump version to 1.0.1

--- misc/bump_version.sh
+++ misc/bump_version.sh
@@ -11,11 +11,9 @@ fi
 # Update CMakeLists.txt
 # This searches for the project() line and replaces the version number
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS version of sed
-    sed -i '' "s/VERSION [0-9.]*/VERSION $NEW_VERSION/" CMakeLists.txt
+    sed -i '' "/project(/ s/VERSION [0-9.]*/VERSION $NEW_VERSION/" CMakeLists.txt
 else
-    # Linux version of sed
-    sed -i "s/VERSION [0-9.]*/VERSION $NEW_VERSION/" CMakeLists.txt
+    sed -i "/project(/ s/VERSION [0-9.]*/VERSION $NEW_VERSION/" CMakeLists.txt
 fi
 
 echo "Updated CMakeLists.txt to version $NEW_VERSION"
