44c1b9af23665e71cad25d7e29f3c6950a76e1ce
Gl1tchs <berkeumtbiricik@gmail.com>
2026-05-28 16:02:22 +0200

    chores: fix FMT_HEADER_ONLY redefinitions

--- include/gpukit/log.h
+++ include/gpukit/log.h
@@ -5,10 +5,12 @@
 #include <cstdint>
 #include <string>
 
-#if defined(GPUKIT_USE_FMT_FORMAT)
+#ifdef GPUKIT_USE_FMT_FORMAT
+#ifndef FMT_HEADER_ONLY
 #define FMT_HEADER_ONLY
-#include <fmt/format.h>
+#endif
 #include <fmt/chrono.h>
+#include <fmt/format.h>
 #define GPUKIT_FMT_STRING(...) fmt::format_string<__VA_ARGS__>
 #define GPUKIT_FMT_FORMAT(f, ...) fmt::format(f, ##__VA_ARGS__)
 #else
@@ -25,28 +27,23 @@ namespace log {
 
 GPUKIT_API void _write(LogLevel level, const std::string& msg);
 
-template <typename... Args>
-void trace(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
+template <typename... Args> void trace(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
 	_write(LogLevel::Trace, GPUKIT_FMT_FORMAT(fmt, std::forward<Args>(args)...));
 }
 
-template <typename... Args>
-void info(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
+template <typename... Args> void info(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
 	_write(LogLevel::Info, GPUKIT_FMT_FORMAT(fmt, std::forward<Args>(args)...));
 }
 
-template <typename... Args>
-void warn(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
+template <typename... Args> void warn(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
 	_write(LogLevel::Warning, GPUKIT_FMT_FORMAT(fmt, std::forward<Args>(args)...));
 }
 
-template <typename... Args>
-void error(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
+template <typename... Args> void error(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
 	_write(LogLevel::Error, GPUKIT_FMT_FORMAT(fmt, std::forward<Args>(args)...));
 }
 
-template <typename... Args>
-void fatal(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
+template <typename... Args> void fatal(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
 	_write(LogLevel::Fatal, GPUKIT_FMT_FORMAT(fmt, std::forward<Args>(args)...));
 }
 
@@ -54,8 +51,8 @@ void fatal(GPUKIT_FMT_STRING(Args...) fmt, Args&&... args) {
 
 } // namespace gpukit
 
-#define GPUKIT_LOG_TRACE(...)   gpukit::log::trace(__VA_ARGS__)
-#define GPUKIT_LOG_INFO(...)    gpukit::log::info(__VA_ARGS__)
+#define GPUKIT_LOG_TRACE(...) gpukit::log::trace(__VA_ARGS__)
+#define GPUKIT_LOG_INFO(...) gpukit::log::info(__VA_ARGS__)
 #define GPUKIT_LOG_WARNING(...) gpukit::log::warn(__VA_ARGS__)
-#define GPUKIT_LOG_ERROR(...)   gpukit::log::error(__VA_ARGS__)
-#define GPUKIT_LOG_FATAL(...)   gpukit::log::fatal(__VA_ARGS__)
+#define GPUKIT_LOG_ERROR(...) gpukit::log::error(__VA_ARGS__)
+#define GPUKIT_LOG_FATAL(...) gpukit::log::fatal(__VA_ARGS__)
